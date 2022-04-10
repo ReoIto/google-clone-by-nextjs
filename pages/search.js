b8996d4efc2ef39685bca0e07567fdcf0e53cac1
Reo Ito <leo.0210.ito@gmail.com>
2022-04-10 16:21:31 +0900

    Created SearchResult component and modify the title in search page

--- pages/search.js
+++ pages/search.js
@@ -1,18 +1,23 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
 import Response from "../Response";
+import SearchResults from "../components/SearchResults";
+import { useRouter } from "next/router";
 
-export default function search({ results }) {
+export default function Search({ results }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
-        <title>Search Page</title>
+        <title>{router.query.term} - Search Page</title>
       </Head>
 
       {/* Search Header */}
       <SearchHeader />
 
       {/* Search Results */}
+      <SearchResults results={results} />
     </div>
   );
 }
