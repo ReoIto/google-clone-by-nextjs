615ba55363ee787f7cdfaf72bdcac68008e3c841
Reo Ito <leo.0210.ito@gmail.com>
2022-04-12 23:05:05 +0900

    Created PaginationComponents

---
+++ components/PaginationButtons.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+  ChevronDoubleLeftIconn,
+  ChevronDoubleRightIcon,
+} from "@heroicons/react/solid";
+
+export default function PaginationButtons() {
+  const router = useRouter();
+
+  return <div>PaginationButtons</div>;
+}
